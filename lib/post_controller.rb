37d1439183d860646f166257640968cafa6cc3ff
Jonathan Avalos <javalos@pernix-solutions.com>
2013-07-09 23:59:34 -0600

    Added route post to posts

--- lib/post_controller.rb
+++ lib/post_controller.rb
@@ -7,14 +7,22 @@ class PostController < WEBrick::HTTPServlet::AbstractServlet
   </posts>"
 
   def do_GET(request, response)
-    status, content_type, body = get_posts(request)
+    status, content_type, body = get_posts
 
     response.status = status
     response['Content-Type'] = content_type
     response.body = body
   end
 
-  def get_posts(request)
+  def do_POST(request, response)
+    status, content_type, body = create_post(request)
+
+    response.status = status
+    response['Content-Type'] = content_type
+    response.body = body
+  end
+
+  def get_posts
     post_source = PostSource.new(TEST_SOURCE)
     posts = post_source.all
     html = "<html><body><div><h1>Posts:</h1>"
@@ -23,5 +31,9 @@ class PostController < WEBrick::HTTPServlet::AbstractServlet
     end
     html = html + "</div></body></html>"
     return 200, "text/html", html
+  end
+
+  def create_post request
+    return 200, "text/html", "html"
   end
 end
