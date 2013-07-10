da42a6a5fa8e06408476ec553cafd1424c8e5c63
Jonathan Avalos <javalos@pernix-solutions.com>
2013-07-09 21:59:15 -0600

    Added post source

--- spec/post_test.rb
+++ spec/post_test.rb
@@ -1,8 +1,14 @@
 class PostTest < TestJavalos
   def test_initialize
-    post = Post.new(source: "")
+    post = Post.new("")
     assert_not_null post
+  end
+
+  def test_post_source
+    source = "<posts><post>Post 1<post><posts>"
+    post = Post.new(source)
     assert_not_null post.source
+    assert_equals source, post.source
   end
 
 end
