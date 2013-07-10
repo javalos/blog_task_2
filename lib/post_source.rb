d0d5686cb5a8a8089e22d0c9d40722b90c214f85
Jonathan Avalos <javalos@pernix-solutions.com>
2013-07-09 23:16:32 -0600

    Added post source all content

--- lib/post_source.rb
+++ lib/post_source.rb
@@ -1,10 +1,20 @@
+require "rexml/document"
+include REXML
+
 class PostSource
   attr_accessor :source
+
   def initialize source
     @source = source
+    doc = Document.new source
+    @posts = Array.new
+    XPath.each(@doc, "/posts/post") do |post|
+      content = post.elements["content"].text
+      @posts << Post.new(content)
+    end
   end
 
   def all
-    [Post.new, Post.new, Post.new]
+    @posts
   end
 end
