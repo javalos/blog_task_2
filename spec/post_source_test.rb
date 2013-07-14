2bac9e45e0649eca8cc426da9955e8b3bd75d075
Jonathan Avalos <javalos@pernix-solutions.com>
2013-07-13 23:24:00 -0600

    added test for create post

--- spec/post_source_test.rb
+++ spec/post_source_test.rb
@@ -28,4 +28,13 @@ class PostSourceTest < TestJavalos
     assert_equals "Post 3", posts[2].content
   end
 
+  def test_post_create
+    post_source = PostSource.new(TEST_SOURCE)
+    posts = post_source.all
+    count_before = posts.count
+    post_source.create Post.new("Another post")
+    posts = post_source.all
+    assert_equals count_before + 1, posts.count
+  end
+
 end
